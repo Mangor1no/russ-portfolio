da6ca2f57b9dd79de790bc7ad5e80d00dae8d823
Mangor1no <tungtm998@gmail.com>
2022-10-09 05:39:19 +0700

    preload fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,10 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
